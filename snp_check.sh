aecaaca14e7af4315451b77982886037c8e4e938
Katie Barnes <katie@sanogenetics.com>
2024-07-16 15:55:29 +0100

    Update names

--- snp_check.sh
+++ snp_check.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 #                                                                              #
-#                      Neuron23 variant checker                                #
+#                      Variant checker                                         #
 #                                                                              #
 # This script processes one or multiple VCF files to check the presence and    #
 # quality of specified SNPs. The results are written to text files with the    #
@@ -11,12 +11,12 @@
 # be written to the current directory.                                         #
 #                                                                              #
 # Usage:                                                                       #
-#   ./neuron23_snp_check.sh <input1.vcf> [input2.vcf] [...] [output_directory] #
+#   ./snp_check.sh <input1.vcf> [input2.vcf] [...] [output_directory]          #
 #                                                                              #
 # Example:                                                                     #
-#   ./neuron23_snp_check.sh sample1.vcf sample2.vcf mydirectory                #
-#   ./neuron23_snp_check.sh *.vcf mydirectory                                  #
-#   ./neuron23_snp_check.sh sample1.vcf                                        #
+#   ./snp_check.sh sample1.vcf sample2.vcf mydirectory                         #
+#   ./snp_check.sh *.vcf mydirectory                                           #
+#   ./snp_check.sh sample1.vcf                                                 #
 #                                                                              #
 ################################################################################
 
